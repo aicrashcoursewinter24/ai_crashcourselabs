a83f6699dc50721c646d21fa3657e1d408ae1b17
LuAmm24 <155859072+LuAmm24@users.noreply.github.com>
2024-01-08 11:38:14 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Labs_on_crashcourse2024',
+    author='Luke',
+    license='MIT',
+)
